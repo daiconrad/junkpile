e65dbebffc798b4c6c7002046c4f0e27695c1ab4
David Conrad <dai.conrad@gmail.com>
2018-06-18 04:43:18 -0400

    Added Nodes to stream NodeLists
    Nodes can stream a NodeList as a stream of Node objects, a stream of
    strings of the node names, or a stream of strings of the text contents.

---
+++ src/java/in/digo/stream/Nodes.java
@@ -0,0 +1,36 @@
+package in.digo.stream;
+
+import static java.util.Objects.requireNonNull;
+
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.NodeList;
+import org.w3c.dom.Node;
+
+public class Nodes {
+    private final NodeList list;
+
+    public Nodes(NodeList list) {
+        this.list = requireNonNull(list);
+    }
+
+    public static Nodes of(NodeList list) {
+        return new Nodes(list);
+    }
+
+    public Stream<Node> stream() {
+        return IntStream.range(0, list.getLength()).mapToObj(list::item);
+    }
+
+    public Stream<String> strings() {
+        return stream().map(Node::getTextContent);
+    }
+
+    public Stream<String> names() {
+        return stream().map(Node::getNodeName);
+    }
+}
